cd7778af1f59a66cf1d32e4900d064f536377c17
TurboSjaantje <daanvdm@hotmail.com>
2021-12-09 11:29:58 +0100

    update

--- src/Day9_Part2.java
+++ src/Day9_Part2.java
@@ -2,37 +2,17 @@ import java.util.ArrayList;
 
 public class Day9_Part2 {
     public static void main(String[] args) {
-        ArrayList<String> input = new importFileLines().getInput("Day9.txt");
-        ArrayList<String[]> inputArrays = new ArrayList<>();
-        int sum = 0;
 
-        for (String string : input) {
-            inputArrays.add(string.split(""));
-        }
+        ArrayList<String> input = new importFileLines().getInput("test.txt");
+        ArrayList<int[]> numberLines = new ArrayList<>();
 
-        for (int i = 0; i < inputArrays.size(); i++) {
-
-            int[] numbers = new int[100];
-            for (int j : numbers) {
-
+        for (String line : input) {
+            String[] numberStrs = line.split("");
+            int[] numbers = new int[numberStrs.length];
+            for (int i = 0; i < numberStrs.length; i++) {
+                numbers[i] = Integer.parseInt(numberStrs[i]);
             }
-
-            for (int j = 0; j < inputArrays.get(i).length; j++) {
-
-                if (j != 0 || j != input.size() - 1) {
-
-                    if (Integer.valueOf(inputArrays.get(i)[j]) < Integer.valueOf(inputArrays.get(i)[j - 1]) && Integer.valueOf(inputArrays.get(i)[j]) < Integer.valueOf(inputArrays.get(i)[j + 1])) {
-
-                    }
-
-                } else if (j == 0) {
-
-                } else if (j == input.size()) {
-
-                }
-
-            }
-
+            numberLines.add(numbers);
         }
 
     }
