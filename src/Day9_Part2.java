8f2af6efd6696b608fc38eeabc05887e17d9f753
TurboSjaantje <daanvdm@hotmail.com>
2021-12-09 11:15:26 +0100

    update

--- src/Day9_Part2.java
+++ src/Day9_Part2.java
@@ -2,40 +2,38 @@ import java.util.ArrayList;
 
 public class Day9_Part2 {
     public static void main(String[] args) {
-//        ArrayList<String> input = new importFileLines().getInput("Day9.txt");
-//        ArrayList<String[]> inputArrays = new ArrayList<>();
-//        int sum = 0;
-//
-//        for (String string : input) {
-//            inputArrays.add(string.split(""));
-//        }
-//
-//        for (int i = 0; i < inputArrays.size(); i++) {
-//
-//            int[] numbers = new int[100];
-//            for (int j : numbers) {
-//
-//            }
-//
-//            for (int j = 0; j < inputArrays.get(i).length; j++) {
-//
-//                if (j != 0 || j != input.size()) {
-//
-//                    if (Integer.valueOf(inputArrays.get(i)[j]) < Integer.valueOf(inputArrays.get(i)[j - 1])
-//                            && Integer.valueOf(inputArrays.get(i)[j]) < Integer.valueOf(inputArrays.get(i)[j + 1]) &&
-//                            Integer.valueOf()) {
-//
-//                    }
-//
-//                } else if (j == 0) {
-//
-//                } else if (j == input.size()) {
-//
-//                }
-//
-//            }
-//
-//        }
+        ArrayList<String> input = new importFileLines().getInput("Day9.txt");
+        ArrayList<String[]> inputArrays = new ArrayList<>();
+        int sum = 0;
+
+        for (String string : input) {
+            inputArrays.add(string.split(""));
+        }
+
+        for (int i = 0; i < inputArrays.size(); i++) {
+
+            int[] numbers = new int[100];
+            for (int j : numbers) {
+
+            }
+
+            for (int j = 0; j < inputArrays.get(i).length; j++) {
+
+                if (j != 0 || j != input.size() - 1) {
+
+                    if (Integer.valueOf(inputArrays.get(i)[j]) < Integer.valueOf(inputArrays.get(i)[j - 1]) && Integer.valueOf(inputArrays.get(i)[j]) < Integer.valueOf(inputArrays.get(i)[j + 1])) {
+
+                    }
+
+                } else if (j == 0) {
+
+                } else if (j == input.size()) {
+
+                }
+
+            }
+
+        }
 
     }
 
